3b9e5b4649bd82ba7c2be490ce1ef54e09a7eb5b
Hasan Aden <kamaaloos@gmail.com>
2026-08-25 05:18:31 +0300

    updated license public key.

--- lib/licensing/license_public_key.dart
+++ lib/licensing/license_public_key.dart
@@ -4,5 +4,5 @@
 /// Regenerate with: `dart run tool/licensing/generate_keypair.dart`
 abstract final class LicensePublicKey {
   static const String ed25519PublicKeyBase64 =
-      'oRlXdLEwg1HuDW4Y++8tKZqzbEtjJ98sjqLJ63uTYQA=';
+      'bQNTnrWYyopA00FQdrAJ/eTYr7V1ndKhQ4kqiEvqCZ8=';
 }
